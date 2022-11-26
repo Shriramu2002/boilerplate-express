e7cbc0163e6c242051cb4faa34fbafd823a1c404
Shriramu2002 <shriramu2002@gmail.com>
2022-11-26 20:45:25 +0530

    2nd' submission

--- myApp.js
+++ myApp.js
@@ -1,9 +1,7 @@
 let express = require('express');
 let app = express();
 app.get("/",function(req,res){
-    if(req.body==="Hello Express"){
-    res.send("Response String");
-    }
+    res.send("Hello Express");
   });
 // console.log("Hello world");
 
