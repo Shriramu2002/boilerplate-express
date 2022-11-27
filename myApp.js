5ff86f3870d3325caf7735765b0815b694d2bf3f
Shriramu2002 <shriramu2002@gmail.com>
2022-11-27 19:43:45 +0530

    7th submission

--- myApp.js
+++ myApp.js
@@ -1,13 +1,16 @@
+const { text } = require('body-parser');
 let express = require('express');
 let app = express();
+require("dotenv").config();
 const path = require("path");
+const str = "Hello json";
 app.use("/public",express.static(path.join(__dirname,"public")));
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname,"views/index.html"));
   });
  app.get("/json",function(req,res){
     res.json({
-        "message":"Hello json"
+        "message":process.env.MESSAGE_STYLE==="uppercase"?str.toUpperCase():str.toLowerCase()
     });
  }) ;
 // console.log("Hello world");
