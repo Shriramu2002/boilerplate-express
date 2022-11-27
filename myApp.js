4fc84723e54af1d737d7b1e8e74c943722969bfc
Shriramu2002 <shriramu2002@gmail.com>
2022-11-27 18:43:10 +0530

    5th submission

--- myApp.js
+++ myApp.js
@@ -1,14 +1,11 @@
 let express = require('express');
 let app = express();
 const path = require("path");
+app.use("/public",express.static(path.join(__dirname,"public")));
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname,"views/index.html"));
   });
 // console.log("Hello world");
-
-
-
-
 
 
 
