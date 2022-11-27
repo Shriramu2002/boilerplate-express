e6d36d0c39cd582b274d2502b020e0124b041fe8
Shriramu2002 <shriramu2002@gmail.com>
2022-11-27 19:20:06 +0530

    6th submission

--- myApp.js
+++ myApp.js
@@ -5,6 +5,11 @@ app.use("/public",express.static(path.join(__dirname,"public")));
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname,"views/index.html"));
   });
+ app.get("/json",function(req,res){
+    res.json({
+        "message":"Hello json"
+    });
+ }) ;
 // console.log("Hello world");
 
 
